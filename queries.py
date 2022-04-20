a1d764cd27fc95ed87d58789efa5da4a4a323e53
Slade Hirschi <sladehirsc@gmail.com>
2022-04-20 00:09:06 -0600

    First commit

--- queries.py
+++ queries.py
@@ -102,12 +102,60 @@ def viewPosts():
                     Commented By: {}
                 Likes {}  Dislikes {}
             """.format(post[3], post[0], post[5], post[4], post[1], post[2]))
+
+def viewFriendsPosts(username):
+    data1 = cur.execute("SELECT id FROM accounts WHERE username = ?", [username])
+    accountId = data1.fetchone()
+    if accountId is not None:
+        id = accountId[0]
+    else:
+        print("This is an invalid username")
+        return
+    data = cur.execute("""
+        SELECT 
+            p.content, 
+            p.likes, 
+            p.dislikes,
+            a1.username as poster,
+            a2.username as commenter,
+            c.content
+        FROM 
+            posts p
+        LEFT JOIN
+            accounts a1
+            ON p.account_id = a1.id
+        LEFT JOIN
+            comments c
+            ON c.post_id = p.id
+        LEFT JOIN
+            accounts a2
+            ON c.account_id = a2.id
+        JOIN
+            friends f
+            ON f.from_id = ? AND p.account_id = f.to_id
+        ORDER BY a1.username
+        """, [id])
+    posts = data.fetchall()
+    print(posts)
+    for post in posts:
+        if post is None:
+            print("There are none")
+            return
+        else:
+            print("""
+                Posted By: {}
+                    Content: {}
+                Comments:
+                    {}
+                    Commented By: {}
+                Likes {}  Dislikes {}
+            """.format(post[3], post[0], post[5], post[4], post[1], post[2]))
     
 
 def getFunction():
     return input("""
     What command would you like to do? 
-        Possible: addUser, addAccount, viewAccounts, addRelation, viewPosts, addPost, addComment, addReply, likeComment, viewPosts)
+        Possible: addUser, addAccount, viewAccounts, addRelation, viewPosts, addPost, addComment, addReply, likeComment, viewFriendsPosts)
 
     TO QUIT enter exit  
 """)
@@ -147,6 +195,11 @@ def main():
             username = input("What is your username? ")
             content = input("What do you want to write in your post? ")
             addPost(username, content)
+            function = getFunction()
+
+        elif function == 'viewFriendsPosts':
+            username = input("What is your username? ")
+            viewFriendsPosts(username)
             function = getFunction()
 
         elif function == 'exit':
